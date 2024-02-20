f640ce1a9421db7244436f162cd959a4bc089e64
NazmulAlamShawon <shawonkst15@gmail.com>
2024-02-20 17:43:51 +0600

    import bug fix

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,6 +1,6 @@
 import  express  from "express";
 import { signup } from "../contollers/auth.controller.js";
-import  signin  from "../contollers/auth.controller.js";
+import  {signin } from "../contollers/auth.controller.js";
 
 const router = express.Router();
 
